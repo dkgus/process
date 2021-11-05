fb4b1f211ebb3ecde830f6dd4c7a206b2b275309
dkgus12dkgus <dkgus12dkgus@naver.com>
2021-11-05 18:01:14 +0900

    base

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (
     <div>
       <Switch>
-          <Route exact path="/register" component={RegisterPage} />
+          {/* <Route exact path="/register" component={RegisterPage} /> */}
+          <RegisterPage />
       </Switch>
     </div>
   );
